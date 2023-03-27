45c419b7005037d5071f374e052d5835faff373a
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-03-27 00:36:01 -0500

    used usecallback with dependency since a function in javascript is closure it can have access to its enviroment variable and you can say remake the function  if one of it's enviroment variable it is relying on changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,30 @@ import DemoOutput from "./components/Demo/Demo";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("APP RUNNING");
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = useCallback(() => {
+    setAllowToggle(true);
+  }, []);
+
+  const disAllowToggleHandler = useCallback(() => {
+    setAllowToggle(false);
   }, []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Toggle ON</Button>
+      <Button onClick={disAllowToggleHandler}>Toggle OFF</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
