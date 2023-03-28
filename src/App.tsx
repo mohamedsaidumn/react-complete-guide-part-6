6aec71301b9c84826b60e09921fab88ab9dd860d
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-03-27 22:41:03 -0500

    Optimizing with useMemo and save data accross component execution

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 import "./App.css";
 import Button from "./components/UI/Button/Button";
-import DemoOutput from "./components/Demo/Demo";
-
+import DemoList from "./components/Demo/DemoList";
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
+  const [listTitle, setListTitle] = useState("My List");
 
-  console.log("APP RUNNING");
-
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) {
-      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-    }
-  }, [allowToggle]);
-
-  const allowToggleHandler = useCallback(() => {
-    setAllowToggle(true);
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
   }, []);
 
-  const disAllowToggleHandler = useCallback(() => {
-    setAllowToggle(false);
-  }, []);
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToggleHandler}>Toggle ON</Button>
-      <Button onClick={disAllowToggleHandler}>Toggle OFF</Button>
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
